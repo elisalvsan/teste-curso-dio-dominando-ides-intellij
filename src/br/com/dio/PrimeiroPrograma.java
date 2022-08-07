a486eb0f8979fd36f9da64bec168df2135d66475
Elisa Andrade <elisalvsan@gmail.com>
2022-08-06 23:18:01 -0300

    criando class livro

--- src/br/com/dio/PrimeiroPrograma.java
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -6,12 +6,48 @@ public class PrimeiroPrograma {
     public static void main(String[] args) {
 
         Gato gato = new Gato();
+        Livro livro = new Livro("Les Miserables (Barnes & Noble Collectible Classics: Omnibus Edition)", 928);
 
         System.out.println(gato);
+        System.out.println(livro);
 
         /*int a = 5;
         int b = 3;
 
         System.out.println("Hello World!" + (a + b));*/
+    }
+}
+
+class Livro {
+    private String nome;
+    private Integer nunpage;
+
+    public Livro(String nome, Integer nunpage) {
+        this.nome = nome;
+        this.nunpage = nunpage;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getNunpage() {
+        return nunpage;
+    }
+
+    public void setNunpage(Integer nunpage) {
+        this.nunpage = nunpage;
+    }
+
+    @Override
+    public String toString() {
+        return "Livro{" +
+                "nome='" + nome + '\'' +
+                ", nunpage=" + nunpage +
+                '}';
     }
 }
